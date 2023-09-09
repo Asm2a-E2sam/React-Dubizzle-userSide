e7682f7f8e7b91ff792d1f99fc4dcabc25cee60c
MohamedSamir30996 <mohamedsamir30996@gmail.com>
2023-09-09 23:34:33 +0300

    Single Page ( Done )

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AppLayout from './AppLayout';
+import SingleProductPage from './Components/SingleProductPage/SingleProductPage';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: '/singlePage', element: <SingleProductPage /> },
+    ]
+  }
+])
 
 function App() {
   return (
     <>
+      <RouterProvider router={router} />
     </>
   )
 }
